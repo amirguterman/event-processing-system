00f4a561462da4689697fe02c82708808311bc90
amirguterman <amirguterman@gmail.com>
2023-07-02 20:17:55 +0300

    add comments to client.js

--- client.js
+++ client.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const http = require('http');
 
+// Function to send event to the server
 function sendEvent(event) {
+  // HTTP request options
   const options = {
     hostname: 'localhost',
     port: 8000,
@@ -13,6 +15,7 @@ function sendEvent(event) {
     }
   };
 
+  // Create HTTP request
   const req = http.request(options, res => {
     console.log(`statusCode: ${res.statusCode}`);
     res.on('data', d => {
@@ -20,22 +23,27 @@ function sendEvent(event) {
     });
   });
 
+  // Log any request errors
   req.on('error', error => {
     console.error(error);
   });
 
+  // Write event data to request body and end request
   req.write(JSON.stringify(event));
   req.end();
 }
 
+// Read events from events.jsonl file
 fs.readFile('events.jsonl', 'utf8', (err, data) => {
   if (err) {
     console.error('Error reading file:', err);
     return;
   }
 
+  // Parse events from file
   const events = data.trim().split('\n').map(line => JSON.parse(line));
 
+  // Send each event to the server
   events.forEach(event => {
     sendEvent(event);
   });
