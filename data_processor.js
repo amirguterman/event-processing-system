510c9bb588c76c83fb4a47656813703fd6f16afb
amirguterman <amirguterman@gmail.com>
2023-07-02 20:12:32 +0300

    Updated data_processor.js to handle atomic transactions and prevent reprocessing of events

--- data_processor.js
+++ data_processor.js
@@ -1,52 +1,106 @@
+// Import required modules
 const pg = require('pg');
 const fs = require('fs');
 const path = require('path');
 
+// Create a new PostgreSQL connection pool
 const pool = new pg.Pool({
   // TODO: Add some PostgreSQL configuration here ......
 });
 
+// Create a directory for processed files if it doesn't exist
+const processedDir = './processed';
+if (!fs.existsSync(processedDir)){
+    fs.mkdirSync(processedDir);
+}
+
+// Read the current directory
 fs.readdir('.', (err, files) => {
+  // If there's an error reading the directory, log it and return
   if (err) {
     console.error('Error reading directory:', err);
     return;
   }
 
+  // Filter out the event files from the list of files
   const eventFiles = files.filter(file => file.startsWith('events-') && file.endsWith('.jsonl'));
 
+  // Process each event file
   eventFiles.forEach(file => {
-    fs.readFile(file, 'utf8', (err, data) => {
+    // Start a transaction
+    pool.query('BEGIN', (err) => {
       if (err) {
-        console.error(`Error reading file ${file}:`, err);
+        console.error('Error starting transaction:', err);
         return;
       }
 
-      const events = data.trim().split('\n').map(line => JSON.parse(line));
-      const revenues = {};
-
-      events.forEach(event => {
-        if (!revenues[event.userId]) {
-          revenues[event.userId] = 0;
+      // Read the event file
+      fs.readFile(file, 'utf8', (err, data) => {
+        // If there's an error reading the file, log it and return
+        if (err) {
+          console.error(`Error reading file ${file}:`, err);
+          return;
         }
 
-        if (event.name === 'add_revenue') {
-          revenues[event.userId] += event.value;
-        } else if (event.name === 'subtract_revenue') {
-          revenues[event.userId] -= event.value;
-        }
-      });
+        // Parse the events from the file
+        const events = data.trim().split('\n').map(line => JSON.parse(line));
+        const revenues = {};
 
-      for (const userId in revenues) {
+        // Calculate the revenue for each user
+        events.forEach(event => {
+          if (!revenues[event.userId]) {
+            revenues[event.userId] = 0;
+          }
+
+          // Add or subtract revenue based on the event name
+          if (event.name === 'add_revenue') {
+            revenues[event.userId] += event.value;
+          } else if (event.name === 'subtract_revenue') {
+            revenues[event.userId] -= event.value;
+          }
+        });
+
+        // Insert or update the revenue for each user in the database
+        for (const userId in revenues) {
+          pool.query(
+            'INSERT INTO users_revenue (user_id, revenue) VALUES ($1, $2) ON CONFLICT (user_id) DO UPDATE SET revenue = users_revenue.revenue + EXCLUDED.revenue',
+            [userId, revenues[userId]],
+            (err, res) => {
+              // If there's an error inserting the data, log it
+              if (err) {
+                console.error('Error inserting data:', err);
+              }
+            }
+          );
+        }
+
+        // Mark the file as processed in the database
         pool.query(
-          'INSERT INTO users_revenue (user_id, revenue) VALUES ($1, $2) ON CONFLICT (user_id) DO UPDATE SET revenue = users_revenue.revenue + EXCLUDED.revenue',
-          [userId, revenues[userId]],
+          'INSERT INTO processed_files (file_name) VALUES ($1)',
+          [file],
           (err, res) => {
+            // If there's an error inserting the data, log it
             if (err) {
-              console.error('Error inserting data:', err);
+              console.error('Error marking file as processed:', err);
             }
           }
         );
-      }
+
+        // Commit the transaction
+        pool.query('COMMIT', (err) => {
+          if (err) {
+            console.error('Error committing transaction:', err);
+            return;
+          }
+
+          // Move the processed file to the processed directory
+          fs.rename(file, path.join(processedDir, file), err => {
+            if (err) {
+              console.error(`Error moving file ${file}:`, err);
+            }
+          });
+        });
+      });
     });
   });
 });
