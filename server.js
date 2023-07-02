f59901ec10d5fcb62bcede86ce46356a289e6c14
amirguterman <amirguterman@gmail.com>
2023-07-02 19:33:05 +0300

    Add POST route for /liveEvent with basic auth

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+app.post('/liveEvent', (req, res) => {
+  const authHeader = req.headers.authorization;
+  
+  if (authHeader !== 'secret') {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+  
+  // TODO: Save event data to a file
+  
+  res.status(200).json({ message: 'Event received' });
+});
+
+
 app.listen(8000, () => {
   console.log('Server is running on port 8000');
 });
