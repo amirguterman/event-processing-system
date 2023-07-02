66244e12cd73be452738668adb91823060831e8b
amirguterman <amirguterman@gmail.com>
2023-07-02 19:54:45 +0300

    Include serveEvent code deleted by accident

--- client.js
+++ client.js
@@ -1,4 +1,32 @@
 const fs = require('fs');
+const http = require('http');
+
+function sendEvent(event) {
+  const options = {
+    hostname: 'localhost',
+    port: 8000,
+    path: '/liveEvent',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'secret'
+    }
+  };
+
+  const req = http.request(options, res => {
+    console.log(`statusCode: ${res.statusCode}`);
+    res.on('data', d => {
+      process.stdout.write(d);
+    });
+  });
+
+  req.on('error', error => {
+    console.error(error);
+  });
+
+  req.write(JSON.stringify(event));
+  req.end();
+}
 
 fs.readFile('events.jsonl', 'utf8', (err, data) => {
   if (err) {
