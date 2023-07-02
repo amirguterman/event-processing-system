137f2af1ac78e09da712dfd50b56df5206e2568d
amirguterman <amirguterman@gmail.com>
2023-07-02 19:49:33 +0300

    Update client to read events from a JSONL file

--- client.js
+++ client.js
@@ -1,39 +1,14 @@
-const http = require('http');
+const fs = require('fs');
 
-function sendEvent(event) {
-  const options = {
-    hostname: 'localhost',
-    port: 8000,
-    path: '/liveEvent',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'secret'
-    }
-  };
+fs.readFile('events.jsonl', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading file:', err);
+    return;
+  }
 
-  const req = http.request(options, res => {
-    console.log(`statusCode: ${res.statusCode}`);
-    res.on('data', d => {
-      process.stdout.write(d);
-    });
-  });
+  const events = data.trim().split('\n').map(line => JSON.parse(line));
 
-  req.on('error', error => {
-    console.error(error);
-  });
-
-  req.write(JSON.stringify(event));
-  req.end();
-}
-
-// Call sendEvent function to send events
-const events = [
-    { userId: 'abc123', name: 'add_revenue', value: 100 },
-    { userId: 'abc123', name: 'subtract_revenue', value: 50 },
-];
-
-events.forEach(event => {
+  events.forEach(event => {
     sendEvent(event);
+  });
 });
-  
