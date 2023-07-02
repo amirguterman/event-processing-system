4a13abc3a6d8a0960b121e5e44f41229ff6410ab
amirguterman <amirguterman@gmail.com>
2023-07-02 19:52:06 +0300

    Update server to serve data from the database

--- server.js
+++ server.js
@@ -7,40 +7,41 @@ app.use(express.json());
 
 app.post('/liveEvent', (req, res) => {
     const authHeader = req.headers.authorization;
-    
-    if (authHeader !== 'secret') {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-    
-    fs.appendFile('events.json', JSON.stringify(req.body) + '\n', err => {
-      if (err) {
-        console.error('Error writing file:', err);
-      }
-    });
-    
-    res.status(200).json({ message: 'Event received' });
-  });
-  
 
-  const pool = new pg.Pool({
-    // TODO: ... add postgresSql configuration here also ..
-  });
-  
-  app.get('/userEvents/:userId', (req, res) => {
-    const userId = req.params.userId;
-    
-    pool.query('SELECT * FROM events WHERE userId = $1', [userId], (err, result) => {
-      if (err) {
-        console.error('Error executing query:', err.stack);
-        return res.status(500).json({ error: 'Internal server error' });
-      }
-      
-      res.status(200).json(result.rows);
+    if (authHeader !== 'secret') {
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    fs.appendFile('events.json', JSON.stringify(req.body) + '\n', err => {
+        if (err) {
+            console.error('Error writing file:', err);
+        }
     });
-  });
-  
+
+    res.status(200).json({ message: 'Event received' });
+});
+
+
+const pool = new pg.Pool({
+    // TODO: ... add postgresSql configuration here also ..
+});
+
+app.get('/userEvents/:userId', (req, res) => {
+    const userId = req.params.userId;
+
+    pool.query('SELECT * FROM users_revenue WHERE user_id = $1', [userId], (err, result) => {
+        if (err) {
+            console.error('Error executing query:', err.stack);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+
+        res.status(200).json(result.rows);
+    });
+});
+
+
 
 
 app.listen(8000, () => {
-  console.log('Server is running on port 8000');
+    console.log('Server is running on port 8000');
 });
