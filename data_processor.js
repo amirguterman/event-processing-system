501c48ecb8a5e5a8b6fbcc506375c45c312c5259
amirguterman <amirguterman@gmail.com>
2023-07-02 20:01:12 +0300

    Update data_processor.js to process all event files

--- data_processor.js
+++ data_processor.js
@@ -1,42 +1,52 @@
 const pg = require('pg');
 const fs = require('fs');
+const path = require('path');
 
 const pool = new pg.Pool({
   // TODO: Add some PostgreSQL configuration here ......
 });
 
-const eventsFile = 'pendingEvents.jsonl';
+fs.readdir('.', (err, files) => {
+  if (err) {
+    console.error('Error reading directory:', err);
+    return;
+  }
 
-fs.readFile(eventsFile, 'utf8', (err, data) => {
-    if (err) {
-        console.error('Error reading file:', err);
+  const eventFiles = files.filter(file => file.startsWith('events-') && file.endsWith('.jsonl'));
+
+  eventFiles.forEach(file => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.error(`Error reading file ${file}:`, err);
         return;
-    }
+      }
 
-    const events = data.trim().split('\n').map(line => JSON.parse(line));
-    const revenues = {};
+      const events = data.trim().split('\n').map(line => JSON.parse(line));
+      const revenues = {};
 
-    events.forEach(event => {
+      events.forEach(event => {
         if (!revenues[event.userId]) {
-        revenues[event.userId] = 0;
+          revenues[event.userId] = 0;
         }
 
         if (event.name === 'add_revenue') {
-        revenues[event.userId] += event.value;
+          revenues[event.userId] += event.value;
         } else if (event.name === 'subtract_revenue') {
-        revenues[event.userId] -= event.value;
+          revenues[event.userId] -= event.value;
         }
-    });
+      });
 
-    for (const userId in revenues) {
+      for (const userId in revenues) {
         pool.query(
-        'INSERT INTO users_revenue (user_id, revenue) VALUES ($1, $2) ON CONFLICT (user_id) DO UPDATE SET revenue = users_revenue.revenue + EXCLUDED.revenue',
-        [userId, revenues[userId]],
-        (err, res) => {
+          'INSERT INTO users_revenue (user_id, revenue) VALUES ($1, $2) ON CONFLICT (user_id) DO UPDATE SET revenue = users_revenue.revenue + EXCLUDED.revenue',
+          [userId, revenues[userId]],
+          (err, res) => {
             if (err) {
-            console.error('Error inserting data:', err);
+              console.error('Error inserting data:', err);
             }
-        }
+          }
         );
-    }
-});  
+      }
+    });
+  });
+});
