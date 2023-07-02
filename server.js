b50f7e012835899003de7ecc9baa436a99a5368c
amirguterman <amirguterman@gmail.com>
2023-07-02 19:35:53 +0300

    Add GET route for /userEvents/:userId

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ app.post('/liveEvent', (req, res) => {
   res.status(200).json({ message: 'Event received' });
 });
 
+app.get('/userEvents/:userId', (req, res) => {
+  const userId = req.params.userId;
+  
+  // TODO: Fetch events from database and return as JSON
+  
+  res.status(200).json({ message: `Events for user ${userId}` });
+});
+
 
 app.listen(8000, () => {
   console.log('Server is running on port 8000');
