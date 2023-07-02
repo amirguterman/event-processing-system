ae03e1800692c61a107077aff92496dc090b3967
amirguterman <amirguterman@gmail.com>
2023-07-02 19:51:42 +0300

    Update data processor to calculate user's revenue and save it to the database

--- data_processor.js
+++ data_processor.js
@@ -5,23 +5,36 @@ const pool = new pg.Pool({
   // TODO: Add some PostgreSQL configuration here ......
 });
 
-fs.readFile('events.json', 'utf8', (err, data) => {
-  if (err) {
-    console.error('Error reading file:', err);
-    return;
-  }
+fs.readFile('events.jsonl', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading file:', err);
+        return;
+    }
 
-  const events = JSON.parse(data);
+    const events = data.trim().split('\n').map(line => JSON.parse(line));
+    const revenues = {};
 
-  events.forEach(event => {
-    pool.query(
-      'INSERT INTO events (userId, eventName, value) VALUES ($1, $2, $3)',
-      [event.userId, event.eventName, event.value],
-      (err, res) => {
-        if (err) {
-          console.error('Error inserting data:', err);
+    events.forEach(event => {
+        if (!revenues[event.userId]) {
+        revenues[event.userId] = 0;
         }
-      }
-    );
-  });
-});
+
+        if (event.name === 'add_revenue') {
+        revenues[event.userId] += event.value;
+        } else if (event.name === 'subtract_revenue') {
+        revenues[event.userId] -= event.value;
+        }
+    });
+
+    for (const userId in revenues) {
+        pool.query(
+        'INSERT INTO users_revenue (user_id, revenue) VALUES ($1, $2) ON CONFLICT (user_id) DO UPDATE SET revenue = users_revenue.revenue + EXCLUDED.revenue',
+        [userId, revenues[userId]],
+        (err, res) => {
+            if (err) {
+            console.error('Error inserting data:', err);
+            }
+        }
+        );
+    }
+});  
